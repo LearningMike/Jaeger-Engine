d39a1eb83cef691803027deb28105b700f05c4c1
Michael Abia <michael.abia@aun.edu.ng>
2022-04-25 14:20:13 +0100

    moved character parameters to a single object

---
+++ game_engine.js
@@ -0,0 +1,181 @@
+// @michael_abia_v1 2022
+// Game Engine Variables and Functions
+
+var Game = {
+    "height": window.innerHeight,
+    "width" : window.innerWidth,
+    "assets" : [],
+    "ready" : false,
+    "grid" : false,
+    "online" : false,
+    "key" : {},
+    "collisionGraph": []
+}
+
+var togglePositions = () => {
+    if (Game.grid == false){
+        Game.grid = true;
+    } else {
+        Game.grid = false;
+    }
+}
+
+var drawCharacter = (cursor, character, characterX, characterY, characterW, characterH) => {
+    cursor.beginPath();
+    cursor.drawImage(character, characterX, characterY, characterW, characterH);
+    cursor.imageSmoothingEnabled = false;
+    if(Game.grid == true){
+        //show position
+        cursor.font = '10px Arial';
+        cursor.fillStyle = '#000000';
+        cursor.fillText('x='+characterX+', y='+characterY, characterX, characterY);
+    }
+}
+
+var graph = (cursor) => {
+    //horizontal lines
+    for (let i = 0; i <= Game.height; i++) {
+        if (i%25 == 0) {
+            cursor.beginPath();
+            cursor.lineWidth = 0.2;
+            cursor.moveTo(0, i);
+            cursor.font = '10px Arial';
+            cursor.fillStyle = '#000000';
+            cursor.fillText(i, 10, i);
+            cursor.lineTo(Game.width, i);
+            cursor.strokeStyle = "#005500";
+            cursor.stroke();   
+        }
+    }
+    //vertical lines
+    for (let i = 0; i <= Game.width; i++) {
+        if (i%25 == 0) {
+            cursor.beginPath();
+            cursor.lineWidth = 0.2;
+            cursor.moveTo(i, 0);
+            cursor.font = '10px Arial';
+            cursor.fillStyle = '#000000';
+            cursor.fillText(i, i, 10);
+            cursor.lineTo(i, Game.height);
+            cursor.strokeStyle = "#005500";
+            cursor.stroke();
+        }
+    }
+}
+
+class Character {
+    constructor (data){
+        this.name = data.name;
+        this.x = data.x;
+        this.y = data.y;
+        this.width = data.width;
+        this.height = data.height;
+        this.direction = data.direction;
+        this.speed = data.speed;
+        this.maxspeed = data.maxspeed;
+        this.minspeed = data.minspeed;
+        this.move = (direction, speed) => {
+            this.speed = speed;
+            this.direction = direction;
+            console.log("speed:"+this.speed);
+            switch (direction) {
+                case 1:
+                    //up
+                    this.y = this.y - speed;
+                    break;
+                case 2:
+                    //up and right
+                    this.y = this.y - speed;
+                    this.x = this.x + speed;
+                    break;
+                case 3:
+                    //right
+                    this.x = this.x + speed;
+                    break;
+                case 4:
+                    //right and down
+                    this.x = this.x + speed;
+                    this.y = this.y + speed;
+                    break;
+                case 5:
+                    //down
+                    this.y = this.y + speed;
+                    break;
+                case 6:
+                    //down and left
+                    this.y = this.y + speed;
+                    this.x = this.x - speed;
+                    break;
+                case 7:
+                    //left
+                    this.x = this.x - speed;
+                    break;
+                case 8:
+                    //left and up
+                    this.x = this.x - speed;
+                    this.y = this.y - speed;
+                    break;
+                default:
+                    break;
+            }
+        }
+        this.mass = data.mass;
+        this.acceleration = data.acceleration;
+        this.applyforce = (direction, amount) => {
+            if (this.speed < this.maxspeed && this.speed >= this.minspeed){
+                this.speed = this.speed + (amount/this.mass);
+            }
+            this.move(direction, this.speed);
+            this.acceleration = amount/this.mass;
+            console.log(direction+" acceleration="+this.acceleration);
+        }
+        this.link = data.link;
+        this.image = new Image();
+        this.physics = data.physics;
+        this.gravity = data.gravity;
+        this.friction = data.friction;
+        this.massphysics = data.massphysics;
+        this.input = data.input;
+        Game.assets.push(this);
+    }
+}
+
+window.onload = () => {
+
+    //flip mobile screens
+    window.mobileAndTabletCheck = function() {
+        let check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    };
+
+    if(window.mobileAndTabletCheck()){
+        document.body.requestFullscreen();
+        screen.orientation.lock('landscape');
+    }
+
+    //load all starting assets images, if assets have multiple images in future do a nested loop
+    for (i = 0; i < Game.assets.length; i++) {
+        const asset = Game.assets[i];
+        const percent = ((i+1)/Game.assets.length)*100;
+        asset.image.src = asset.link;
+        asset.image.onload = () => {
+            console.log(asset.name+" loaded "+percent+"% complete");
+            if (percent == 100){
+                Game.ready = true;
+            }
+        }
+    }
+};
+
+window.onkeydown = input = (e) => {
+
+    Game.key[e.code] = true;
+
+};
+
+window.onkeyup = input = (e) => {
+
+    Game.key[e.code] = false;
+
+};
--- gamelogic.js
+++ gamelogic.js
@@ -1,236 +1,84 @@
+// @michael_abia_v1 2022
+// Game Logic Created with Game Engine
 
+//#Background
 
-// Game Engine Variables and Functions
-var height = 150;
-var width = 300;
-var assets = [];
-var ready = false;
-var started = false;
-var grid = false;
-var togglePositions = () => {
-    if (grid == false){
-        grid = true;
-    } else {
-        grid = false;
-    }
-}
+//#Collision
+
+//#NonPlayers
+
+//#Players
+var playerOne = new Character({
+    'name':'mario',
+    'x':Game.width/2,
+    'y':Game.height/2,
+    'width':20,
+    'height':20,
+    'direction':0,
+    'speed':0,
+    'maxspeed':10,
+    'minspeed':0,
+    'mass':10,
+    'acceleration':0,
+    'link':'pic1.png',
+    'physics':true,
+    'gravity':0.9,
+    'friction':0.7,
+    'massphysics':false,
+    'input':true
+});
+
+//#Frame
 var drawGame = () => {
     window.requestAnimationFrame(drawGame);
     var canvas = document.getElementById("screen");
     var cursor = canvas.getContext("2d");
-    cursor.clearRect(0, 0, 300, 150);
-    //draw background assets first, physics objects last
-    if (grid == true) {
-        //draw graph
+    cursor.canvas.width = Game.width;
+    cursor.canvas.height = Game.height;
+    cursor.clearRect(0, 0, Game.width, Game.height);
+
+    if (Game.grid == true) {
         graph(cursor);
     }
-    assets.forEach(character => {
-        drawCharacter(cursor, character.image, character.x, character.y);
+
+    Game.assets.forEach(character => {
+
+        if (character.input == true){
+            if (Game.key['KeyW']){
+                character.applyforce(1, 1);
+            }
+            if (Game.key['KeyA']){
+                character.applyforce(7, 1);
+            }
+            if (Game.key['KeyS']){
+                character.applyforce(5, 1);
+            }
+            if (Game.key['KeyD']){
+                character.applyforce(3, 1);
+            }
+        }
+
+        if (character.physics == true){
+            //inertia
+            character.applyforce(character.direction, character.mass*character.acceleration);
+            //gravity
+            character.applyforce(5, character.mass*character.gravity);
+            //collision
+            Game.collisionGraph.push([character.x, character.y, character.height, character.width]);
+            Game.collisionGraph.forEach(collider =>{
+                if (collider.x <= (character.x+character.width) && (collider.x+collider.width) >= character.x && collider.y <= (character.y+character.height) && (collider.y+collider.height) >= character.y){
+                    //calculate momentum before and after collision
+                    //apply force to both colliders (bounce or no bounce? i mean should the collision be elastic?)
+                }
+            });
+            //friction
+        }
+        if (character.massphysics == true){
+            //planetary physics, fluid simulations? have fun
+        }
+        if (Game.online == true){
+            //stream positions of players to each other
+        }
+        drawCharacter(cursor, character.image, character.x, character.y, character.width, character.height);
     });
-    started = true;
 }
-var drawCharacter = (cursor, character, characterX, characterY) => {
-    cursor.beginPath();
-    cursor.drawImage(character, characterX, characterY, 20, 20);
-    cursor.imageSmoothingEnabled = false;
-    if(grid == true){
-        //show position
-        cursor.font = '10px Arial';
-        cursor.fillStyle = '#000000';
-        cursor.fillText('x='+characterX+', y='+characterY, characterX, characterY);
-    }
-}
-var graph = (cursor) => {
-    //horizontal lines
-    for (let i = 0; i <= height; i++) {
-        if (i%20 == 0) {
-            cursor.beginPath();
-            cursor.lineWidth = 0.2;
-            cursor.moveTo(0, i);
-            cursor.font = '10px Arial';
-            cursor.fillStyle = '#000000';
-            cursor.fillText(i, 10, i);
-            cursor.lineTo(width, i);
-            cursor.strokeStyle = "#009900";
-            cursor.stroke();   
-        }
-    }
-    //vertical lines
-    for (let i = 0; i <= width; i++) {
-        if (i%20 == 0) {
-            cursor.beginPath();
-            cursor.lineWidth = 0.2;
-            cursor.moveTo(i, 0);
-            cursor.font = '10px Arial';
-            cursor.fillStyle = '#000000';
-            cursor.fillText(i, i, 10);
-            cursor.lineTo(i, height);
-            cursor.strokeStyle = "#009900";
-            cursor.stroke();
-        }
-    }
-}
-
-class Character {
-    constructor(name, x, y, mass, link, physics){
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        this.direction = 0;
-        this.speed = 0;
-        this.maxspeed = 10;
-        this.minspeed = 0;
-        this.move = (direction, speed) => {
-            this.speed = speed;
-            this.direction = direction;
-            console.log("speed:"+this.speed);
-            switch (direction) {
-                case 1:
-                    //up
-                    this.y = this.y - speed;
-                    break;
-                case 2:
-                    //up and right
-                    this.y = this.y - speed;
-                    this.x = this.x + speed;
-                    break;
-                case 3:
-                    //right
-                    this.x = this.x + speed;
-                    break;
-                case 4:
-                    //right and down
-                    this.x = this.x + speed;
-                    this.y = this.y + speed;
-                    break;
-                case 5:
-                    //down
-                    this.y = this.y + speed;
-                    break;
-                case 6:
-                    //down and left
-                    this.y = this.y + speed;
-                    this.x = this.x - speed;
-                    break;
-                case 7:
-                    //left
-                    this.x = this.x - speed;
-                    break;
-                case 8:
-                    //left and up
-                    this.x = this.x - speed;
-                    this.y = this.y - speed;
-                    break;
-                default:
-                    break;
-            }
-        }
-        this.mass = mass;
-        this.acceleration = 0;
-        this.applyforce = (direction, amount) => {
-            if (this.speed < this.maxspeed){
-                this.speed = this.speed + (amount/this.mass);
-            }
-            this.move(direction, this.speed);
-            this.acceleration = amount/this.mass;
-            console.log(direction+" acceleration="+this.acceleration);
-        }
-        this.link = link;
-        this.image = new Image();
-        this.physics = physics;
-        this.gravity = 9.8;
-        this.friction = 0.7;
-        assets.push(this);
-    }
-}
-
-//create characters\\ this is what the editor should create and run
-var playerOne = new Character('mario', (width/2)-50, height/2, 10, 'pic1.png', true);
-var playerTwo = new Character('luigi', (width/2)+50, height/2, 10, 'pic2.png', false);
-
-//load all assets, if assets have multiple images in future do a nested loop
-for (i = 0; i < assets.length; i++) {
-    const asset = assets[i];
-    const percent = ((i+1)/assets.length)*100;
-    asset.image.src = asset.link;
-    asset.image.onload = () => {
-        console.log(asset.name+" loaded "+percent+"% complete");
-        if (percent == 100){
-            ready = true;
-        }
-    }
-}
-
-window.onload = () => {
-
-    //Characters behave according to #logic, #input and #inputstreams
-    
-    //#LOGIC
-    setInterval(() => {
-        if (started == true) {
-            //Physics
-            assets.forEach(asset => {
-                if (asset.physics == true){
-                    //inertia
-                    asset.applyforce(asset.direction, asset.mass*asset.acceleration);
-                    //gravity
-                    //collision
-                    //friction
-                }
-                if (asset.collision == true){
-                    //
-                }
-                if (asset.massphysics == true){
-                    //planetary physics, have fun
-                }
-            }); 
-        }
-    }, 167);
-    
-    //#INPUT
-    window.onkeydown = input = (e) => {
-        switch (e.code) {
-            case 'Space':
-                // Start Game
-                if (ready == true){
-                    drawGame();
-                } else {
-                    console.log("asset load not complete");
-                }
-                break;
-            case 'KeyW':
-                playerOne.applyforce(1, 1);
-                break;
-            case 'KeyA':
-                playerOne.applyforce(7, 1);
-                break;
-            case 'KeyS':
-                playerOne.applyforce(5, 1);
-                break;
-            case 'KeyD':
-                playerOne.applyforce(3, 1);
-                break;
-            case 'ArrowUp':
-                playerTwo.move(1, 5);
-                break;
-            case 'ArrowLeft':
-                playerTwo.move(7, 5);
-                break;
-            case 'ArrowDown':
-                playerTwo.move(5, 5);
-                break;
-            case 'ArrowRight':
-                playerTwo.move(3, 5);
-                break;
-            case 'KeyP':
-                togglePositions();
-                break;
-        
-            default:
-                break;
-        }
-    };
-
-    //#INPUTSTREAMS
-    //get position from realtime database or sockets and update it
-};
