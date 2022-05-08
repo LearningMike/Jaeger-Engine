0d6d60b66f6c4a40f35ecf16664059626f23cd49
Michael Abia <michael.abia@aun.edu.ng>
2022-05-08 18:19:00 +0100

    made way for tabs

---
+++ public/assets/example.jsx
@@ -0,0 +1,4 @@
+var name = "yunus"; //word variable
+var age = 10; //number variable
+var names = ["yunus", "mohammed", "murtala"]; // array or list variable
+var ages = [10, 9, 9]; // array or list variable
